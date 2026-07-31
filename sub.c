2c14c2b6066caacb6a0fb37af108aafacf467169
kunalsharmacse2025-lang <kunal.sharma.cse.2025@miet.ac.in>
2026-07-31 11:28:49 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a[2][3],b[2][3],i,j,c[2][3];
+    for(i=0;i<2;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<2;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(i=0;i<2;i++){
+        for(j=0;j<3;j++){
+            c[i][j]=a[i][j]-b[i][j];
+        }
+    }
+    for(i=0;i<2;i++){
+        for(j=0;j<3;j++){
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
+}
